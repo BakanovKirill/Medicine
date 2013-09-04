5352ed2374c8e494051a69bb59d1954f50e6a91f
bakanovkirill <kirill.bakanov@gmail.com>
2013-09-04 17:00:37 +0300

    added management command to print patients

---
+++ src/medicine/management/commands/print_patients.py
@@ -0,0 +1,18 @@
+### -*- coding: utf-8 -*- ####################################################
+
+from django.core.management.base import BaseCommand
+from django.db.models import F
+from django.db.models.aggregates import Sum
+from medicine.models import Patient
+
+class Command(BaseCommand):
+    '''Print to console all patients(__unicode__ value)'''
+    args = ''
+    help = 'Prints all patients from database to console'
+    
+    def handle(self, nodelete=False, *args, **options):
+        
+        for patient in Patient.objects.all():
+            #Print patients
+            print('Patients list:\n')
+            print(patient)
